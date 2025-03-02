ef47b765c4c6831f3ea5711b4146b891a2648346
faisal-din <dahrifaisal56@gmail.com>
2025-03-02 19:47:46 +0500

    added collection page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Collection from './pages/Collection';
+import Collection from './Pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Product from './Pages/Product';
@@ -19,22 +19,22 @@ console.log(toast);
 
 function App() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <ToastContainer />
 
       <Navbar />
       <SearchBar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/orders" element={<Orders />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
 
       <Footer />
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../assets/assets';
 import { Link, NavLink } from 'react-router-dom';
-import { ShopContext } from '../gitContext/ShopContext';
+import { ShopContext } from '../Context/ShopContext';
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  // const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -34,9 +34,9 @@ const Navbar = () => {
 
       <div className='flex items-center gap-6'>
         <img
-          // onClick={() => {
-          //   setShowSearch(true);
-          // }}
+          onClick={() => {
+            setShowSearch(true);
+          }}
           src={assets.search_icon}
           alt=''
           className='w-5 cursor-pointer '
