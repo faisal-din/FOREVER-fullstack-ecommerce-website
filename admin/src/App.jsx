3afaf3232c65e4f31b7e2a0c7417d1362565ec29
faisal-din <dahrifaisal56@gmail.com>
2025-03-03 18:03:23 +0500

    added admin navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
-  return <h1 class='text-3xl font-bold '>ADMIN PANEL</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
