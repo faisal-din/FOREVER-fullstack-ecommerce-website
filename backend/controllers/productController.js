59e15c4df64afbd881e2e0f950411bfead83cffc
faisal-din <dahrifaisal56@gmail.com>
2025-03-03 12:02:44 +0500

    added add product to database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,69 @@
+import { v2 as cloudinary } from 'cloudinary';
+import ProductModel from '../models/productModel.js';
+
 export const addProduct = async (req, res) => {
-  res.send('Add Product');
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
+
+    // Check if images are availabe in req.files then it`ll store image in image variable
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    // Filter out undefined images
+    const images = [image1, image2, image3, image4].filter(
+      (image) => image !== undefined
+    );
+
+    // Check if images are not available then it`ll throw an error
+    if (images.length === 0) {
+      throw new Error('Please upload at least one image');
+    }
+
+    let imageUrl = await Promise.all(
+      images.map(async (image) => {
+        const result = await cloudinary.uploader.upload(image.path, {
+          resource_type: 'image',
+        });
+        return result.secure_url;
+      })
+    );
+
+    const productData = {
+      name,
+      description,
+      price: Number(price),
+      category,
+      subCategory,
+      sizes: JSON.parse(sizes),
+      bestseller: bestseller === 'true' ? true : false,
+      image: imageUrl,
+      date: Date.now(),
+    };
+
+    const product = new ProductModel(productData);
+    await product.save();
+
+    res.status(201).json({
+      success: true,
+      message: 'Product added successfully',
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const listProducts = async (req, res) => {
