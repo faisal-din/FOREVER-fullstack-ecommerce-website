11efe6a25d6c2a9ff34a629576c40eebc6adf632
faisal-din <dahrifaisal56@gmail.com>
2025-03-05 10:41:13 +0500

    added  uplaoading and displaying the image on the screen using the image path from the input field

--- admin/src/Pages/Add.jsx
+++ admin/src/Pages/Add.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import { assets } from '../assets/assets';
 
 const Add = () => {
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [subCategory, setSubCategory] = useState('Men');
+  const [price, setPrice] = useState('Topwear');
+  const [sizes, setSizes] = useState([]);
+  const [bestSeller, setBestSeller] = useState(false);
+
   return (
     <form className='flex flex-col w-full items-start gap-3'>
       {/* Upload Image */}
@@ -12,34 +25,62 @@ const Add = () => {
           <label htmlFor='image1'>
             <img
               className='w-20 cursor-pointer'
-              src={assets.upload_area}
+              src={!image1 ? assets.upload_area : URL.createObjectURL(image1)}
               alt=''
             />
-            <input type='file' id='image1' hidden />
+            <input
+              onChange={(e) => {
+                setImage1(e.target.files[0]);
+              }}
+              type='file'
+              id='image1'
+              hidden
+            />
           </label>
           <label htmlFor='image2'>
             <img
               className='w-20 cursor-pointer'
-              src={assets.upload_area}
+              src={!image2 ? assets.upload_area : URL.createObjectURL(image2)}
               alt=''
             />
-            <input type='file' id='image2' hidden />
+            <input
+              onChange={(e) => {
+                setImage2(e.target.files[0]);
+              }}
+              type='file'
+              id='image2'
+              hidden
+            />
           </label>
           <label htmlFor='image3'>
             <img
               className='w-20 cursor-pointer'
-              src={assets.upload_area}
+              src={!image3 ? assets.upload_area : URL.createObjectURL(image3)}
               alt=''
             />
-            <input type='file' id='image3' hidden />
+            <input
+              onChange={(e) => {
+                setImage3(e.target.files[0]);
+              }}
+              type='file'
+              id='image3'
+              hidden
+            />
           </label>
           <label htmlFor='image4'>
             <img
               className='w-20 cursor-pointer'
-              src={assets.upload_area}
+              src={!image4 ? assets.upload_area : URL.createObjectURL(image4)}
               alt=''
             />
-            <input type='file' id='image4' hidden />
+            <input
+              onChange={(e) => {
+                setImage4(e.target.files[0]);
+              }}
+              type='file'
+              id='image4'
+              hidden
+            />
           </label>
         </div>
       </div>
