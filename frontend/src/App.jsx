b160ef9a5916696df1366df4cb1f40cc95e5fb74
faisal-din <dahrifaisal56@gmail.com>
2025-03-10 09:39:29 +0500

    fix home page issue in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import Home from './Pages/Home';
 import Collection from './Pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
